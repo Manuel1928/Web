7deb85b6f766d6c8f95e8feb078d00da2590b116
Manuel1928 <manuel1998angel@gmail.com>
2022-09-30 13:50:28 -0500

    update

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -4,11 +4,10 @@ import graphql_jwt
 import links.schema
 import users.schema
 
-
-class Query(users.schema.Query,links.schema.Query, graphene.ObjectType):
+class Query(links.schema.Query, users.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(users.schema.Mutation,links.schema.Mutation, graphene.ObjectType):
+class Mutation(links.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
